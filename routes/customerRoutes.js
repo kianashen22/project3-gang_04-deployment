329e4a63ae3f5aba8ff5a84305c8f7b7723e7b6d
Julian-Hon <julianhon@tamu.edu>
2025-11-16 22:11:37 -0600

    added weather api

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -4,7 +4,7 @@ const express = require('express');
 const { Pool } = require('pg');
 const dotenv = require('dotenv').config();
 const session = require('express-session');
-
+const axios = require('axios');
 
 // create image arrays
 // milky image will be the default drink image when a new drink is created unless added to the array
@@ -78,9 +78,46 @@ router.use((req, res, next) => {
 
 
 // Routes for customer pages
-router.get('/customerHome', (req, res) => {
+router.get('/customerHome', async (req, res) => {
+  try {
     console.log("Customer homepage hit!");
-    res.render('customer/customerHome');
+
+    const city = req.query.city || 'Houston';
+
+    const apiKey = process.env.OPENWEATHER_API_KEY;
+    console.log('OpenWeather key present:', !!apiKey);
+
+    const weatherResponse = await axios.get(
+      'https://api.openweathermap.org/data/2.5/weather',
+      {
+        params: {
+          q: city,
+          appid: apiKey,
+          units: 'imperial',
+        },
+      }
+    );
+
+    const data = {
+      city: weatherResponse.data.name,
+      temp: weatherResponse.data.main.temp,
+      feelsLike: weatherResponse.data.main.feels_like,
+      description: weatherResponse.data.weather[0].description,
+    };
+
+    res.render('customer/customerHome', { weather: data, error: null });
+  } catch (err) {
+    if (err.response) {
+      console.error('OpenWeather error:', err.response.status, err.response.data);
+    } else {
+      console.error('Unknown error:', err.message);
+    }
+
+    res.render('customer/customerHome', {
+      weather: null,
+      error: 'Error fetching weather.',
+    });
+  }
 });
 
 
